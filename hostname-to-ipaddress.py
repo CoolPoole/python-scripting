d37e7462fa15686354846bc76e351ec783760089
spoole <spoole34@gmail.com>
2020-08-24 21:12:54 -0400

    added hostname to output to accompany ip address

--- hostname-to-ipaddress.py
+++ hostname-to-ipaddress.py
@@ -2,12 +2,12 @@
 import socket
 
 # use the open function to read a file of hostnames (i.e. open(file, mode))
-# then use gethostbyname to translate hostname to ip address
+# then use gethostbyname to translate hostname to ip address and output to screen
 # strip() method will remove any leading or trailing characters
 with open("hostnames.txt", "r") as ins:
     for line in ins:
         ip_address = socket.gethostbyname(line.strip())
-        print(ip_address)
+        print(line, ip_address)
         
         
 # can replace lines 9 and 10 with this statement: print(socket.gethostbyname(line.strip()))
